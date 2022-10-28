8fc43ff18f70ee3daa982285f1bb5a3b3b25ff99
Haitao Chen <haitch@microsoft.com>
2022-10-28 01:45:54 -0700

    resource for ref

--- graph/template.go
+++ graph/template.go
@@ -4,6 +4,9 @@ import (
 	"text/template"
 )
 
+// https://www.graphviz.org/docs/
+// http://magjac.com/graphviz-visual-editor/
+
 var digraphTemplate = template.Must(template.New("digraph").Parse(digraphTemplateText))
 
 const digraphTemplateText = `digraph {
@@ -11,5 +14,13 @@ const digraphTemplateText = `digraph {
 	newrank = "true"
 	subgraph "root" {
 {{ range $from, $toList := $}}{{ range $_, $to := $toList}}		"{{$from}}" -> "{{$to}}"
+{{ end }}{{ end }}        }
+}`
+
+const digraphTemplateTextV2 = `digraph {
+	compound = "true"
+	newrank = "true"
+	subgraph "root" {
+{{ range $node := $.Nodes}}		"{{$node.Name}}" [label="{{$node.Name}}", shape="{{node.Shape}}"]
 {{ end }}{{ end }}        }
 }`
